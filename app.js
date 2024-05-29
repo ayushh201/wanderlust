98c60d94724773e1a4851fd7d77820555329e67f
Ayush Sharma <ayushsharma@Ayushs-MacBook-Air.local>
2024-05-29 14:29:42 +0530

    deploy

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 const Listing = require("./models/listing.js");
+const path = require("path");
+
 
 main()
 .then(() => {
@@ -14,15 +16,17 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req,res) => {
     res.send("root is working");
 });
 
-app.get("/listings", (req,res) => {
-    Listing.find({}).then((res) => {
-        console.log(res);
+app.get("/listings", async (req,res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
     });
-});
 
 app.listen(8080, () => {
     console.log("server is listening on port 8080");
