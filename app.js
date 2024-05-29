2a774cdb487d6bb9326330b2900b52eb40b2c883
Ayush Sharma <ayushsharma@Ayushs-MacBook-Air.local>
2024-05-29 18:08:11 +0530

    REST CRUD OPERATIONS PERFORMED

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 const Listing = require("./models/listing.js");
 const path = require("path");
-
+const methodOverride = require("method-override");
 
 main()
 .then(() => {
@@ -19,6 +19,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 
 app.get("/", (req,res) => {
@@ -45,6 +46,28 @@ app.get("/listings/:id", async (req,res) => {
 app.post("/listings", async (req,res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    res.redirect("/listings");
+});
+
+//edit route
+app.get("/listings/:id/edit", async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+//update route
+app.put("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
     res.redirect("/listings");
 });
 
